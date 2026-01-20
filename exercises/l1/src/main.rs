72e111638096f6a33437f16981d1e9561c4652e4
zhangyemengren <624593758@qq.com>
2026-01-20 20:24:59 +0800

    update

--- exercises/l1/src/main.rs
+++ exercises/l1/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let b = "□";
+    let w = "■";
+    let grid = [
+        [b, b, b],
+        [b, w, b],
+        [b, b, b],
+    ];
+    for row in grid {
+        for cell in row {
+            print!(" {cell} ");
+        }
+        println!();
+    }
 }
